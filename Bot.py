77750324c7fe30bd79c50a33d34ada21d9943586
Beandream <beandreamed@gmail.com>
2018-08-18 14:21:37 -0400

    Test Bot

---
+++ Bot.py
@@ -0,0 +1,22 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run('NDgwMTY3Mzk0MTMyNzU0NDg0.Dlnp8Q.yv-guK2GUkaT8-8-g9CPF7A0rvQ')
