271b70b754edc75c45a9759a8835533b77d2025f
Beandream <beandreamed@gmail.com>
2018-08-19 16:25:05 -0400

    Test

--- Bot.py
+++ Bot.py
@@ -14,6 +14,10 @@ async def on_message(message):
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
+    
+    if message.content.startswith('!game'):
+        runGame(message)
+
 
 
 @client.event
